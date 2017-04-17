999d9b35a69682dc09a1ac3788534bc44d9dc3c4
windsting999 <windsting999@gmail.com>
2017-04-17 22:50:53 +0800

    add a vulcan tool

---
+++ vulcan.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import sys
+import SimpleHTTPServer
+import SocketServer
+
+# print sys.argv
+
+PORT = 8000
+if len(sys.argv) > 1 :
+	PORT = int(sys.argv[1])
+
+Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+
+httpd = SocketServer.TCPServer(("", PORT), Handler)
+
+print "serving at port", PORT
+httpd.serve_forever()
+
